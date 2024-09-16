4f93f420823ad1d7d97246a8cf2ac7c054a97404
Elliissa01 <isa.herode@gmail.com>
2024-09-16 18:34:38 -0400

    Display instruction to player

--- Projet_Chifoumi/main.cpp
+++ Projet_Chifoumi/main.cpp
@@ -1,7 +1,28 @@
+/**************************************/
+/*  Project : PIERRE-FEUILLE-CISEAUX  */ 
+/*  Author : Elliissa01               */
+/*  Date : 2024                       */
+/**************************************/
+
 #include <iostream>
+#include <string>
 using namespace std;
 
+//Functions.
+void displayInstruction();
+
+//Global variable.
+enum Choix {PIERRE, FEUILLE, CISEAUX};
+
+//Main function.
 int main() {
-	cout << "Hello, World!" << endl;
+	displayInstruction();
 	return 0;
+}
+
+//Display instruction to player.
+void displayInstruction() {
+	cout << "=====================" << endl;
+	cout << " Tape 1 pour PIERRE " << endl << " Tape 2 pour FEUILLE " << endl << " Tape 3 pour CISEAUX " << endl;
+	cout << "=====================" << endl;
 }
