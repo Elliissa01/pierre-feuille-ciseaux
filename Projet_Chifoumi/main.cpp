768f6b67c04ca901814db13034b6cc4b51134df8
Elliissa01 <isa.herode@gmail.com>
2024-09-16 20:33:08 -0400

    Reveal the choices in text

--- Projet_Chifoumi/main.cpp
+++ Projet_Chifoumi/main.cpp
@@ -10,13 +10,16 @@
 #include<cstdlib>
 #include<ctime>
 
+//Global variable.
+enum Option { PIERRE, FEUILLE, CISEAUX };
+
 //Functions.
 void displayInstruction();
 int askVerifyChoice();
 int randomChoice();
-
-//Global variable.
-enum Choix {PIERRE, FEUILLE, CISEAUX};
+std::string displayOption(int choice);
+void revealGame(int playerChoice, int computerChoice);
+Option translateNumber(int choice);
 
 //Main function.
 int main() {
@@ -25,29 +28,36 @@ int main() {
 	displayInstruction();
 	playerChoice = askVerifyChoice();
 	computerChoice = randomChoice();
+	revealGame(playerChoice, computerChoice);
 
 	return 0;
 }
 
 //Display instruction to player.
 void displayInstruction() {
-	std::cout << "=====================" << std::endl;
-	std::cout << " Tape 0 pour PIERRE " << std::endl << " Tape 1 pour FEUILLE " << std::endl << " Tape 2 pour CISEAUX " << std::endl;
-	std::cout << "=====================" << std::endl;
+	std::cout << "======================" << std::endl;
+	std::cout << " Tapez 0 pour PIERRE " << std::endl << " Tapez 1 pour FEUILLE " << std::endl << " Tapez 2 pour CISEAUX " << std::endl;
+	std::cout << "======================" << std::endl;
 }
 
 //Ask choice to player and verify it.
 int askVerifyChoice() {
 	int choice;
 	
-	std::cout << std::endl << "--> CHOIX : ";
+	std::cout << std::endl << " CHOIX : ";
 	std::cin >> choice;
+	std::cout << std::endl;
 	
 	while (!std::cin.good() || (choice != 0 && choice != 1 && choice != 2)) {
+		//Ignore the remaining input.
 		std::cin.clear();
-		std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); //Ignore the remaining input.
-		std::cout << std::endl << "--> CHOIX : ";
+		std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+		//Ask again.
+		std::cout << " Choix invalide." << std::endl;
+		displayInstruction();
+		std::cout << std::endl << " CHOIX : ";
 		std::cin >> choice;
+		std::cout << std::endl;
 	}
 
 	return choice;
@@ -59,4 +69,42 @@ int randomChoice() {
 	int choice = std::rand() % 3;
 
 	return choice;
+}
+
+//Display the option as text.
+std::string displayOption(int choice) {
+	std::string option = "";
+	
+	switch (choice) {
+		case 0:
+			option = "PIERRE";
+			break;
+		case 1:
+			option = "FEUILLE";
+			break;
+		case 2:
+			option = "CISEAUX";
+			break;
+	}
+
+	return option;
+}
+
+//Reveal the choices made.
+void revealGame(int playerChoice, int computerChoice) {
+	std::cout << " VOUS --> |" << displayOption(playerChoice) << "|   VS   |" << displayOption(computerChoice) << "| <-- ORDI" <<  std::endl ;
+}
+
+//Translate number in option.
+Option translateNumber(int choice) {
+	Option option;
+
+	switch (choice) {
+	case 0:
+		return option = PIERRE;
+	case 1:
+		return option = FEUILLE;
+	case 2:
+		return option = CISEAUX;
+	}
 }
